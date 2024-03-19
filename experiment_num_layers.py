4f88937dd02f54eb6c988e5b1df30444feb55e14
unknown <kishah@ucsd.edu>
2024-03-18 19:40:01 -0700

    Add automated script

---
+++ experiment_num_layers.py
@@ -0,0 +1,9 @@
+import subprocess
+
+for num_layers in range(1, 6):
+    cmd = (f"python main.py --dataset uci-msg --lr 0.01 --maml_lr 0.008 --drop_rate 0.16 --window_num 8 --num_layers {num_layers} --cuda_device 0")
+    out = subprocess.check_output(cmd, shell=True)
+    results = out.decode().split('\n')[-2]
+    print(f"{num_layers} layers", results)
+
+ #python main.py --dataset uci-msg --lr 0.01 --maml_lr 0.008 --drop_rate 0.16 --window_num 8 --num_layers 2--cuda_device 0
